df947292af29f336c7a397ef6f75d9cb3988ab5a
jagadeshwaran <jagadeshjk7@gmail.com>
2016-02-09 13:57:14 +0530

    Update SNAPPycodeNodeA.py

--- SNAPPycodeNodeA.py
+++ SNAPPycodeNodeA.py
@@ -2,8 +2,6 @@ from synapse.switchboard import *
 
 @setHook(HOOK_STARTUP)
 def startupEvent():
-    initUart(1, 9600) # <= put your desired baudrate here!
-    flowControl(1, False) # <= set flow control False as it is not needed
     uniConnect(DS_TRANSPARENT, DS_STDIO) #From DS_TRANSPARENT to DS_STDIO
     
 @setHook(HOOK_STDIN)    
